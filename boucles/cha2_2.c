2934f2df1f89317780afe76fe29abbe5c489e115
cajty <131008615+cajty@users.noreply.github.com>
2023-09-16 02:46:15 +0100

    you

--- boucles/cha2_2.c
+++ boucles/cha2_2.c
@@ -3,16 +3,17 @@ int main (){
   int num;
     printf("entre nombre :\n");
     scanf("%d",&num);
-    
-    for(int i= 0;i < num;i++){
-        for (int e = 0; e <= num - (i * 2); e++)
+ 
+    for(int i= 0;i <= num;i++){
+        
+     for (int c = 0; c < num - i ; c++)
         {
-            printf(" ");
+            printf("#");
         }
 
-        for (int a = 0; a <= i * 2; a++)
+        for (int a = 0; a <= i * 2 ; a++)
         {
-            printf("\*");
+            printf("*");
         }
         
         printf("\n");
