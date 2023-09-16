7d596e4012fc38fe402d4aa3ebc3b3eab88d7b00
cajty <131008615+cajty@users.noreply.github.com>
2023-09-16 23:23:52 +0100

    s'il est premier ou non

---
+++ boucles/cha2_2.C
@@ -0,0 +1,21 @@
+  #include <stdio.h>
+int main (){
+  int num;
+    printf("entre nombre :\n");
+    scanf("%d",&num);
+ 
+    for(int i= 0;i <= num;i++){
+        
+     for (int c = 0; c < num - i ; c++)
+        {
+            printf(" ");
+        }
+
+        for (int a = 0; a <= i * 2 ; a++)
+        {
+            printf("*");
+        }
+        
+        printf("\n");
+    }
+}
