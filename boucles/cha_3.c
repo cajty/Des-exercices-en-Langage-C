025819a51c87d804b095a4695bcd4f9f5a0eac0e
cajty <fdshjgjgffjhd@gmail.com>
2023-09-16 18:35:00 +0100

    Des exercices en Langage C

--- boucles/cha_3.c
+++ boucles/cha_3.c
@@ -1,27 +1,22 @@
 #include <stdio.h>
+#include <math.h>
 int main (){
   int num;
     printf("entre nombre :\n");
     scanf("%d", &num);
-    
-    /* for(int i= 2 ; i < num;i++){
+    int a = sqrt(num);
+    if(num == 2 || num == 1){
+        printf("prmier\n");
+    }
+    else if () {
+      for(int i=2 ;i <= a ; i++){
+      if(num % i == 0 ){
+          printf("%d NOT",num);;
+      }
       
-       if( (num % i) != 0){
-         printf("%d pair \n",num);
-         break;
-      }else
-       printf("%d not \n",num);
     }
-    //printf("%d premier \n", num); */
-    if ( num == 2 || num ==)
-    for(int i=2 ;i <= num/2 ; i++){
-      if(num % i != 0 ){
-        printf("%d est premier",num);;
-      }
-      if(num == i){
-        printf("%d est n'est premier",num);
-      }
-    }
+    
+    
     return 0;
 }
 
